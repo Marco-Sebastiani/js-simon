d4b015cc5d47b463304bb6111aaf06ccc36ca804
Marco.seb <marco.seba96@gmail.com>
2021-01-14 16:08:32 +0100

    UPGRADE

--- js/script.js
+++ js/script.js
@@ -5,29 +5,58 @@
 // sono stati inseriti i 5 numeri, il software dice quanti e
 // quali dei numeri da indovinare sono stati individuati
 
-var memoryNumbers = [];
+//QUI POPOLO L'ARRAY CON LA FUNZIONE GENERA RANDOM
+var numeriRandom = popolaArray();
 
+//QUI FACCIO COMPARIRE I NUMERI ALL'UTENTE CON UN ALERT
+alert('REGOLE DEl GIOCO \n' + 'MEMORIZZATI QUESTI NUMERI \n' + '\n' + numeriRandom);
 
-while (memoryNumbers.length < 5 ){
-    var number = generaRandom(1,100);
-    if(memoryNumbers.includes(number) == false){
-        memoryNumbers.push(number);
-    }   
+console.log(numeriRandom);
+
+//QUI CON UN CICLO CONTROLLO SE I NUMERI DELL'UTENTE SONO GIA' PRESENTI E SE SONO GIUSTI
+var userNumber = [];
+var correctNumbers = [];
+var wrongNumbers = [];
+
+for(var i = 0; i < 5; i++){
+    var user = parseInt(prompt('inserisi i numeri visti'));
+    if( userNumber.includes(user) == false){
+        userNumber.push(user);
+        if(numeriRandom.includes(user)){
+            correctNumbers.push(user);
+        } else{
+        wrongNumbers.push(user)
+        }     
+    }
+
 }
 
-alert(memoryNumbers);
+console.log(userNumber + ' numeri inseriti');
+console.log(correctNumbers + ' numeri indovinati');
+console.log(wrongNumbers + ' numeri sbabliati');
+// var secondi = 5;
+// var tempoDietro = setTimeout(ciao,5000);
 
 
 //********FUNZIONI********
-
 
 function generaRandom(min, max){
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
 
-
-
-function ciao(){
-
+function popolaArray(){
+    var memoryNumbers = [];
+    while (memoryNumbers.length < 5 ){
+        var number = generaRandom(1,100);
+        if(memoryNumbers.includes(number) == false){
+            memoryNumbers.push(number);
+        }   
+    }
+    return  memoryNumbers;
 }
+
+
+
+
+
